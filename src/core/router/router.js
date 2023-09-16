4deca03cb84532109b5284b6f1a4de73019e31c5
Alexandr Alexeev <alexandr-alexeev5452@rambler.ru>
2023-09-16 16:04:25 +0300

    Routing + layout

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -1,16 +1,23 @@
+import { Layout } from '@/components/layout/layout.component';
 import { NotFound } from '@/components/screens/not-found/not-found.component';
 import { ROUTES } from './routes.data';
 
 export class Router {
-	#routes;
-	#currentRoute;
+	//* Массив путей
+	#routes = ROUTES;
+	//* Текущий путь
+	#currentRoute = null;
+	//* Разворачивали ли мы layout
+	#layout = null;
 
 	constructor() {
-		//* Массив путей
-		this.#routes = ROUTES;
-		//* Текущий путь
-		this.#currentRoute = null;
+		//* Событие при нажатии на стрелочки <= => в поисковой строке
+		window.addEventListener('popstate', () => {
+			this.#handleRouteChange();
+		});
+
 		this.#handleRouteChange();
+		this.#handleLinks();
 	}
 
 	//* Получаем адрес из адресной строки
@@ -20,7 +27,7 @@ export class Router {
 
 	//* Метод для изменения страницы
 	#handleRouteChange() {
-		//* Получаем путь
+		//* Получаем текущий путь из адресной строки
 		const path = this.getCurrentPath() || '/';
 		//* Ищем путь в массиве routes
 		let route = this.#routes.find(route => route.path === path);
@@ -32,12 +39,39 @@ export class Router {
 			};
 		}
 
+		//* Текущий элемент (элемент массива routes (object))
 		this.#currentRoute = route;
-		this.render();
+		this.#render();
 	}
 
-	render() {
+	//* Обработчик на все ссылки. Убирает дефолтное действие (перезагрузку страницы) и навигирует на адрес внутри ссылки
+	#handleLinks() {
+		document.addEventListener('click', event => {
+			const target = event.target.closest('a');
+			if (target) {
+				event.preventDefault();
+				this.navigate(target.href);
+			}
+		});
+	}
+
+	//* Если путь в кликнутой ссылке не равен текущему, меняем его в поисковой строке и изменяем контент страницы
+	navigate(path) {
+		if (path !== this.getCurrentPath()) {
+			window.history.pushState({}, '', path);
+			this.#handleRouteChange();
+		}
+	}
+
+	#render() {
+		//* Создаем экземпляр класса (экран(screen)) из массива routes
 		const component = new this.#currentRoute.component();
-		document.querySelector('#app').innerHTML = component.render();
+
+		if (!this.#layout) {
+			this.#layout = new Layout({ router: this, children: component.render() });
+			document.querySelector('#app').innerHTML = this.#layout.render();
+		} else {
+			document.querySelector('main').innerHTML = component.render();
+		}
 	}
 }
