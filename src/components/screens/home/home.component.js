771b56371c61120f0f247352de84cb3536bb9c35
Alexandr Alexeev <alexandr-alexeev5452@rambler.ru>
2023-09-17 13:00:41 +0300

    RQuery(find, css)

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,6 +1,7 @@
 import { BaseScreen } from '@/core/component/base-screen.component';
 import renderService from '@/core/services/render.service';
 
+import { $R } from '@/core/rquery/rquery.lib';
 import styles from './home.module.scss';
 import template from './home.template.html';
 
@@ -11,6 +12,7 @@ export class Home extends BaseScreen {
 
 	render() {
 		const element = renderService.htmlToElement(template, [], styles);
+		$R(element).find('h1').css('color', 'blue');
 		return element.outerHTML;
 	}
 }
