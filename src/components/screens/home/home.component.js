9c3e737df0dd6007f74dfc51982a0a2e0ae1a9c6
Alexandr Alexeev <alexandr-alexeev5452@rambler.ru>
2023-09-18 13:44:24 +0300

    Ui(loader, heading, user-item). RQuery(text, attr). UserItem(update())

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -2,6 +2,7 @@ import { BaseScreen } from '@/core/component/base-screen.component';
 import renderService from '@/core/services/render.service';
 
 import { Field } from '@/components/ui/field/field.component';
+import { UserItem } from '@/components/ui/user-item/user-item.component';
 import { $R } from '@/core/rquery/rquery.lib';
 import styles from './home.module.scss';
 import template from './home.template.html';
@@ -19,7 +20,16 @@ export class Home extends BaseScreen {
 					name: 'test',
 					placeholder: 'Enter your email',
 					variant: 'green'
-				})
+				}),
+				new UserItem(
+					{
+						avatarPath:
+							'https://avatars.mds.yandex.net/i?id=0f1ff2bcf942e2c2288543ed98c910e6e43ae2dd-9181886-images-thumbs&n=13',
+						name: 'Sasha'
+					},
+					false,
+					() => alert('Hey')
+				)
 			],
 			styles
 		);
